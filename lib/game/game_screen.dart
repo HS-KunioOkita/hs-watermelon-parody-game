92db5c9c58a3aae84aa3dd60e510c51391f8b3cc
hs_okita.kunio <okita.kunio@heartsoft.co.jp>
2024-11-28 13:58:38 +0900

    メニューを追加

--- lib/game/game_screen.dart
+++ lib/game/game_screen.dart
@@ -14,6 +14,32 @@ class GameScreen extends StatelessWidget {
             Navigator.pop(context);
           },
         ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {
+              showMenu(
+                context: context,
+                position: RelativeRect.fromLTRB(100, 100, 0, 0),
+                items: [
+                  PopupMenuItem<int>(
+                    value: 0,
+                    child: Text('Menu Item 1'),
+                  ),
+                  PopupMenuItem<int>(
+                    value: 1,
+                    child: Text('Menu Item 2'),
+                  ),
+                  PopupMenuItem<int>(
+                    value: 2,
+                    child: Text('Menu Item 3'),
+                  ),
+                ],
+                elevation: 8.0,
+              );
+            },
+          ),
+        ],
       ),
       body: GameWidget(
         game: MyGame(),
