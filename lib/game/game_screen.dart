036423198e9abd15d38dcd7bf92ab0ff0f06fde0
ito-shizuru <ito.shizuru@heartsoft.co.jp>
2025-02-20 13:16:06 +0900

    フルーツクラス実装 & TODOコメント追加

--- lib/game/game_screen.dart
+++ lib/game/game_screen.dart
@@ -2,41 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:hs_watermelon_parody_game/view/top_page.dart';
 import 'package:flame/game.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
+import 'package:flame/events.dart';
+import 'dart:async';
 
 class GameScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
-    final game = Game(screenSize: screenSize);
+    final game = GameObject(screenSize: screenSize);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Game Screen'),
+        title: const Text('Game Screen'),
         leading: IconButton(
-          icon: Icon(Icons.arrow_back),
+          icon: const Icon(Icons.arrow_back),
           onPressed: () {
             Navigator.pop(context);
           },
         ),
         actions: [
           IconButton(
-            icon: Icon(Icons.menu),
+            icon: const Icon(Icons.menu),
             onPressed: () {
               showMenu(
                 context: context,
-                position: RelativeRect.fromLTRB(100, 100, 0, 0),
+                position: const RelativeRect.fromLTRB(100, 100, 0, 0),
                 items: [
                   PopupMenuItem<int>(
                     value: 0,
-                    child: Text('Menu'),
+                    child: const Text('メニューへ'),
                     onTap: () {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => TopPage()),
+                        MaterialPageRoute(builder: (context) => const TopPage()),
                       );
                     },
                   ),
                 ],
-                elevation: 8.0,
               );
             },
           ),
@@ -49,38 +51,37 @@ class GameScreen extends StatelessWidget {
             top: 0,
             left: 0,
             child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                // Score表示
                 ValueListenableBuilder<int>(
                   valueListenable: game.score,
                   builder: (context, score, _) {
                     return Text(
-                      'Score \n $score',
-                      style: TextStyle(fontSize: 20, color: Colors.white),
+                      'Score\n $score',
+                      style: const TextStyle(fontSize: 20, color: Colors.white),
                     );
                   },
                 ),
-                SizedBox(width: screenSize.width/10),
-                // BestScore表示
+                const Padding(padding: EdgeInsets.all(10.0)),
                 ValueListenableBuilder<int>(
                   valueListenable: game.bestScore,
                   builder: (context, bestScore, _) {
                     return Text(
                       'BestScore \n $bestScore',
+                      style: const TextStyle(fontSize: 20, color: Colors.white),
+                    );
+                  },
+                ),
+                const Padding(padding: EdgeInsets.all(10.0)),
+                ValueListenableBuilder<int>(
+                  valueListenable: game.bestScore,
+                  builder: (context, bestScore, _) {
+                    return const Text(
+                      '次のボール', // TODO:次のボールを表示させるようにする
                       style: TextStyle(fontSize: 20, color: Colors.white),
                     );
                   },
                 ),
-              // TODO:次のボールを表示させる
-                // ValueListenableBuilder<int>(
-                //   valueListenable: game.bestScore,
-                //   builder: (context, bestScore, _) {
-                //     return Text(
-                //       'BestScore \n $bestScore',
-                //       style: TextStyle(fontSize: 20, color: Colors.white),
-                //     );
-                //   },
-                // ),
               ],
             ),
           ),
@@ -89,54 +90,106 @@ class GameScreen extends StatelessWidget {
     );
   }
 }
+
+
 // ゲームクラス
-class Game extends Forge2DGame {
-  final screenSize;
-  Game({required this.screenSize});
+class GameObject extends Forge2DGame with TapDetector {
+  final Size screenSize;
+  // TODO:計算した点数を代入する
   final score = ValueNotifier<int>(0);
+
+  // TODO:ベストスコアを表示する(Serverからデータ取得する？)
   final bestScore = ValueNotifier<int>(100);
+
+  GameObject({required this.screenSize});
+
   @override
   Future<void> onLoad() async {
-    await super.onLoad();
-    
-    await add(Wall(Vector2(50, 250), Vector2(50, 500)));   // 左の壁
-    await add(Wall(Vector2(300, 500), Vector2(300, 250))); // 右の壁
-    await add(Wall(Vector2(50, 500), Vector2(300, 500)));  // 下の壁
+    world.gravity = Vector2(0, 0);
+    // 壁を追加
+    await add(Wall(Vector2(screenSize.width*0.2, screenSize.height*0.2), Vector2(screenSize.width*0.2, screenSize.height*0.5))); // 左の壁
+    await add(Wall(Vector2(screenSize.width*0.8, screenSize.height*0.5), Vector2(screenSize.width*0.8, screenSize.height*0.2))); // 右の壁
+    await add(Wall(Vector2(screenSize.width*0.2, screenSize.height*0.5), Vector2(screenSize.width*0.8, screenSize.height*0.5))); // 下の壁
 
-    // await add(Wall(Vector2(screenSize.width/10, screenSize.height-700), Vector2(screenSize.width/10, screenSize.height-400)));   // 左の壁
+    // 最初のボール表示
+    add(Fruits(Vector2(screenSize.width*0.5, screenSize.height*0.15)));
+    pawnBallPeriodically();
+  }
+  var  x = 0.0;
+  // ボール表示メソッド
+  void ballDrawing(Timer timer) {
+    x += 1.0;
+    world.gravity = Vector2(0, 0);
+    add(Fruits(Vector2(screenSize.width*0.5 + x, screenSize.height*0.15)));
+  }
 
+  // 一定間隔でボールを表示
+  // TODO: ランダムで違う数字のボールを表示させる
+  void pawnBallPeriodically() {
+    Timer.periodic(const Duration(seconds: 5), ballDrawing);
+  }
+  // TODO:一定時間経過後重力を発生させる(ボールを落とす)
+
+  // TODO:左右にボールを動かす処理を入れる
+
+  // TODO:点数計算処理を入れる
+
+  // タップした時に重力を設定して、ボールを落とす
+  @override
+  void onTapDown(TapDownInfo info) {
+    world.gravity = Vector2(0, 50);
   }
 }
-// 壁クラス
-class Wall extends BodyComponent {
-  final Vector2 startingPosition;
-  final Vector2 endPosition;
-  late final Body body;
-  Wall(this.startingPosition, this.endPosition);
+
+// フルーツクラス
+interface class Fruits extends BodyComponent {
   @override
-  Future<void> onLoad() async {
-    body = createBody();
-  }
+  final Vector2 position;
+  Fruits(this.position);
+
   @override
   Body createBody() {
-    final length = (endPosition.y - startingPosition.y);
-    // 反射時の形状(どのような形状のあたり判定をつけるか)
-    final shape = PolygonShape()..setAsBox(5, length, Vector2.zero(), 0);
-    // 反射率
-    final reflection = FixtureDef(shape)..restitution = 0.8;
-
-    final bodyDef = BodyDef(position: Vector2(0, 0), type: BodyType.static);
-    return world.createBody(bodyDef)..createFixture(reflection);
+    final shape = CircleShape()..radius = 10;
+    final fixtureDef = FixtureDef(shape)
+    ..density = 0.5
+    ..restitution = 0.5;
+    final bodyDef = BodyDef(position: position, type: BodyType.dynamic);
+    return world.createBody(bodyDef)..createFixture(fixtureDef);
   }
+
+  // ボールのレイアウト設定メソッド
   @override
   void render(Canvas canvas) {
-    // 壁の色、幅の設定
+    // TODO:画像を設定できるようにする
+    final paint = Paint()..color = Colors.green;
+    canvas.drawCircle(Offset.zero, 10, paint);
+  }
+}
+
+class Cloud {
+
+}
+
+// 壁クラス
+class Wall extends BodyComponent {
+  final Vector2 startPosition, endPosition;
+  Wall(this.startPosition, this.endPosition);
+
+  @override
+  Body createBody() {
+    //  TODO:左右の壁の当たり判定をつける
+    final length = (endPosition.x - startPosition.x);
+    // 長方形の当たり判定をつける(横：下の壁の長さ、縦：線の太さ*2、中心点：下の壁の中心、角度：0度)
+    final shape = PolygonShape()..setAsBox(length, 10, Vector2(length/2,startPosition.y), 0);
+    // 反射率
+    final fixtureDef = FixtureDef(shape)..restitution = 0.2;
+    final bodyDef = BodyDef(position: Vector2.zero(), type: BodyType.static);
+    return world.createBody(bodyDef)..createFixture(fixtureDef);
+  }
+
+  @override
+  void render(Canvas canvas) {
     final paint = Paint()..color = Colors.white..strokeWidth = 5;
-    canvas.drawLine(
-      // 壁を描画する位置の設定
-      Offset(startingPosition.x, startingPosition.y),
-      Offset(endPosition.x, endPosition.y),
-      paint,
-    );
+    canvas.drawLine(Offset(startPosition.x, startPosition.y), Offset(endPosition.x, endPosition.y), paint);
   }
 }
